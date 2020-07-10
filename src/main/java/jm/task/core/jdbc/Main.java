08ab178587d7bec3c365ecd2f0d99e7f97670989
IrekNabiullin <08git08@mail.ru>
2020-07-10 19:38:29 +0300

    Set connection and statement in class Util. And do connection in main class.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Util util = new Util();
+        try {
+            util.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                util.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static Connection connection = null;
+    public static Statement statement = null;
+    private static final String URL = "jdbc:mysql://localhost:3306/testbase";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "root";
+
+    public static void connect() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("Driver loaded successfully");
+
+        connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+
+        if (!connection.isClosed()) {
+            System.out.println("Connected to DB");
+        }
+
+        if (connection.isClosed()) {
+            System.out.println("Connection closed");
+        }
+
+        statement = connection.createStatement();
+    }
+
+    public static void disconnect() throws SQLException {
+        statement.close();
+        connection.close();
+    }
+
 }

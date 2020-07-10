e92fd53c2db340247a39a825eb1405915f09eac5
IrekNabiullin <08git08@mail.ru>
2020-07-10 19:54:22 +0300

    Instantination of utility class 'Util' removed

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,14 +5,13 @@ import jm.task.core.jdbc.util.Util;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        Util util = new Util();
         try {
-            util.connect();
+            Util.connect();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
-                util.disconnect();
+                Util.disconnect();
             } catch (Exception e) {
                 e.printStackTrace();
             }
